fe41e771d0f2b6811a4e12859d005d071221b5d9
jyolx <isac.jyolsnaj@gmail.com>
2024-05-16 10:25:46 +0530

    rectify view function for event/add/

--- src/app1/urls.py
+++ src/app1/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.events,name='Home-Page'),
     path("accounts/", include("django.contrib.auth.urls")),
-    path('event/add/', views.event,name='addevent'),
+    path('event/add/', views.add_event,name='addevent'),
     path('event/listvenue/', views.list_venue, name='listvenue'),
     re_path('events/(?P<selector>[0-9a-zA-Z]+)/$', views.events),
     path('events/', views.events),
