639f3b22bba9c02f03b5e018ce1d6f3ef822ecfe
Avanee Lakare <avaneelakare@gmail.com>
2024-05-16 12:54:07 +0530

    done with events page!

--- src/app1/urls.py
+++ src/app1/urls.py
@@ -5,8 +5,9 @@ from . import views
 urlpatterns = [
     path('', views.events,name='Home-Page'),
     path("accounts/", include("django.contrib.auth.urls")),
-    path('event/add/', views.add_event,name='addevent'),
-    path('event/listvenue/', views.list_venue, name='listvenue'),
-    re_path('events/(?P<selector>[0-9a-zA-Z]+)/$', views.events),
+    path('events/add/', views.add_event,name='addevent'),
+    path('events/listvenue/', views.list_venue, name='listvenue'),
+    re_path('events/(?P<datestr>[0-9a-zA-Z]+)/(?P<selector>[0-9a-zA-Z]+)/$', views.events),
+    re_path('events/(?P<datestr>[0-9a-zA-Z]+)/$', views.events),
     path('events/', views.events),
 ]
