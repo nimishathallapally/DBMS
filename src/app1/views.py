081a1a62827b9e393713d7be9850f479d91c3bab
Avanee Lakare <avaneelakare@gmail.com>
2024-05-16 10:09:23 +0530

    Upcoming events in progress

--- src/app1/views.py
+++ src/app1/views.py
@@ -22,10 +22,10 @@ def add_event(request):
 def events(request, selector='all'):
 
     if selector == 'all':
-        events = Event.objects.filter(event_start_date_time__date=timezone.now())
+        events = Event.objects.filter(event_start_date_time__date=timezone.now()).order_by('event_start_date_time')
     
     else:
-        events = Event.objects.filter(dept_id=selector, event_start_date_time__date=timezone.now())
+        events = Event.objects.filter(dept_id=selector, event_start_date_time__date=timezone.now()).order_by('event_start_date_time')
 
     template = loader.get_template('app1/events.html')
     logging.warn(f'Events:{events}')
@@ -40,7 +40,7 @@ def get_agenda():
     agenda={}
     for i in range(1,8):
         curr_date = timezone.now()+relativedelta(days=i)
-        agenda[curr_date] = Event.objects.filter(event_start_date_time__date=curr_date)
+        agenda[curr_date] = Event.objects.filter(event_start_date_time__date=curr_date).order_by('event_start_date_time')
     logging.warn(f'Agenda:{agenda}')
     return agenda
 
